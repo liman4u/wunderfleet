1a1141ade52423b503affc98c445ec87331c0dee
Liman Adamu Labaran <limanadamu@172-6-3-119.lightspeed.irvnca.sbcglobal.net>
2018-10-10 15:06:42 +0000

    Creates controller with route for home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: limanadamu
+ * Date: 10/10/2018
+ * Time: 3:02 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    public function index() {
+
+        return view('index');
+    }
+
+    public function storePersonalInformation() {
+
+
+    }
+
+    public function storeAddressInformation() {
+
+
+    }
+
+    public function storePaymentInformation() {
+
+
+    }
+
+}
